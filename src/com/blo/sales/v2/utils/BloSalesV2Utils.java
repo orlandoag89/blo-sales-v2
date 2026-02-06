4b04e715352b8fa0e31e91be42f68ade02c69fbf
Orlando Angeles <30941319+orlandoag89@users.noreply.github.com>
2026-02-06 13:51:21 -0600

    [CHORE] version 2.5.2-RELEASE

--- src/com/blo/sales/v2/utils/BloSalesV2Utils.java
+++ src/com/blo/sales/v2/utils/BloSalesV2Utils.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
  */
 public final class BloSalesV2Utils {
     
-    public static final String VERSION = "v2.5.2";
+    public static final String VERSION = "v2.5.2-RELEASE";
     
     /** cadena vacia */
     public static final String EMPTY_STRING = "";
@@ -163,3 +163,4 @@ public final class BloSalesV2Utils {
         return onlyDate.toString();
     }
 }
+
