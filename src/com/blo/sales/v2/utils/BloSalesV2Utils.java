49fe2558236617169b3dd990c67f03d518c4d020
Orlando Angeles <30941319+orlandoag89@users.noreply.github.com>
2026-02-15 13:42:04 -0600

    [CHORE] 2.5.7-RELEASE

--- src/com/blo/sales/v2/utils/BloSalesV2Utils.java
+++ src/com/blo/sales/v2/utils/BloSalesV2Utils.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
  */
 public final class BloSalesV2Utils {
     
-    public static final String VERSION = "v2.5.7-SNAPSHOT";
+    public static final String VERSION = "v2.5.7-RELEASE";
     
     /** cadena vacia */
     public static final String EMPTY_STRING = "";
@@ -178,6 +178,7 @@ public final class BloSalesV2Utils {
     }
     
 }
+
 
 
 
